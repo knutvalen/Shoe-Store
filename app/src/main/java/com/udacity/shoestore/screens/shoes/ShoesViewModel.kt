8125fe2cdba0525e398390ee0451d733bc8588be
Knut Valen <knut.valen@icloud.com>
2020-11-18 21:32:12 +0100

    add ShoesViewModel

---
+++ app/src/main/java/com/udacity/shoestore/screens/shoes/ShoesViewModel.kt
@@ -0,0 +1,20 @@
+package com.udacity.shoestore.screens.shoes
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class ShoesViewModel : ViewModel() {
+
+    private val _shoes = MutableLiveData<MutableList<Shoe>>()
+    val shoes: LiveData<MutableList<Shoe>>
+        get() = _shoes
+
+    init {
+        _shoes.value = mutableListOf(
+
+        )
+    }
+
+}
