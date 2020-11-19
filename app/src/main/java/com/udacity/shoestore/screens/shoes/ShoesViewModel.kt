bae9c07266e7de415c7895dc1768c8d9562dacf2
Knut Valen <knut.valen@icloud.com>
2020-11-19 09:42:12 +0100

    add Adidas Superstar shoe

--- app/src/main/java/com/udacity/shoestore/screens/shoes/ShoesViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoes/ShoesViewModel.kt
@@ -13,7 +13,20 @@ class ShoesViewModel : ViewModel() {
 
     init {
         _shoes.value = mutableListOf(
-
+            Shoe(
+                "Superstar",
+                44.0,
+                "Adidas",
+                "THE AUTHENTIC LOW TOP WITH THE SHELL TOE.\n" +
+                    "\n" +
+                    "Originally made for basketball courts in the '70s. Celebrated by hip hop " +
+                        "royalty in the '80s. The adidas Superstar shoe is now a lifestyle staple " +
+                        "for streetwear enthusiasts. The world-famous shell toe feature remains, " +
+                        "providing style and protection. Just like it did on the B-ball courts back " +
+                        "in the day. Now, whether at a festival or walking in the street you can " +
+                        "enjoy yourself without the fear of being stepped on. The serrated 3-Stripes " +
+                        "detail and adidas Superstar box logo adds OG authenticity to your look."
+            )
         )
     }
 
